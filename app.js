e15e856178919dfaa94e1a70bf15a56b49d7a798
Amalynn <amandine.ebner@gmail.com>
2025-02-26 18:29:02 +0100

    Adding MongoDB  database connection

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 app.use(express.json());
+
+mongoose.connect('mongodb+srv://amandine:admin@cluster0.am0m3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
