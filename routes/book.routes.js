89f62a95164f282b987481052059b307952cc4c0
Amalynn <amandine.ebner@gmail.com>
2025-03-05 20:15:20 +0100

    auth.js middleware added

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const bookController = require('../controllers/book.controller');
+const auth = require('../middlewares/auth');
 
 router.get('/:id', bookController.getOneBook);
 router.get('/bestrating', bookController.getBestRatingBooks);
-router.post('/', bookController.createBook);
-router.put('/:id', bookController.updateBook);
-router.delete('/:id', bookController.deleteBook);
-router.post('/:id/rating', bookController.createRatingBook);
+router.post('/', auth, bookController.createBook);
+router.put('/:id', auth, bookController.updateBook);
+router.delete('/:id', auth, bookController.deleteBook);
+router.post('/:id/rating', auth, bookController.createRatingBook);
 router.get('/',bookController.getAllBooks);
 
 module.exports = router;
