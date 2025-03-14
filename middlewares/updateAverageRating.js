c8144a481bc37cccd6ef7b7d9aacb04cfc736e8c
Amalynn <amandine.ebner@gmail.com>
2025-03-14 12:33:18 +0100

    Fixing of the problems in the  updateAverageRating.js middleware

--- middlewares/updateAverageRating.js
+++ middlewares/updateAverageRating.js
@@ -1,20 +1,24 @@
 module.exports = async (req, res) => {
     try {
-        const book = req.book;
+        const book = req.book;        
 
         if(!book) {
             return res.status(404).json({message: "Livre non trouvé"});
         }
-        
+    
         const numberOfNotes = book.ratings.length;
-        const sumOfNotes = book.ratings.reduce((acc, currentValue) => acc + currentValue.grade, 0);
-       
-        book.averageRating = sumOfNotes / numberOfNotes ;
-        
-        await book.save();
-        res.status(200).json({book});
+        const sumOfNotes = book.ratings.reduce((acc, currentValue) => acc + currentValue.grade, 0); 
+        const mean =  sumOfNotes / numberOfNotes;      
+    
+        book.averageRating = parseFloat(mean.toFixed(1));
+    
+        const updatedBook = await book.save();
 
+        if(updatedBook) {
+            res.status(201).json(updatedBook)
+        }        
+        
     } catch (error) {
-        res.status(500).json({error});
-    }     
+        res.status(500).json({error})
+    }    
 };
